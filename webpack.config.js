69d6cc4a0c038459638260658cfb61d3bc4f4b0a
Cyrus <cyrussmith2017@gmail.com>
2018-03-07 15:02:46 +0100

    i did the first react

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require("path");
-
-var config = {
- entry: './src/main.js',
+const path = require("path");var config = {
+ entry: './src/index.jsx',
  output: {
    path: path.resolve(__dirname, "dist"),
-   filename: 'bundle.js',
+   filename: 'bundle.js'
  },
- plugins: [
-   new HtmlWebpackPlugin({
-     template: '!!pug-loader!src/index.pug',
-   }),
- ]
+
+ module: {
+   loaders: [
+     {
+       test: /\.jsx?$/,
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['es2015', 'react']
+       }
+     }
+   ]
+ },
+ plugins: [new HtmlWebpackPlugin({
+     minify: {
+       collapseWhitespace: true
+     },
+     title: 'Pug Test',
+     posts: [
+       {
+         name: 'Tomatoe',
+         price: 1.50
+       }, {
+         name: 'carrots',
+         price: 2
+       }, {
+         name: 'apple',
+         price: 4
+       }
+     ],
+     template: '!!pug-loader!src/index.pug'
+   })]
 }
 module.exports = config;
